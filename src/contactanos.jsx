ac2f6af3ec92beb20ae6c0083397b3f0322a8fe2
RCO04 <rodrigogameelcrack@gmail.com>
2024-03-09 23:00:02 +0100

    moviendo y reseña a la pgaina de contacto y añadiendo enlae a esta

--- src/contactanos.jsx
+++ src/contactanos.jsx
@@ -1,13 +1,14 @@
 
 import { Footer } from "./footer";
 import { Cabecera } from './cabecera';
+import { Reseñas } from "./reseñas";
 
 function Contactanos() {
     return (
         <>
             <Cabecera />
             <form className="mx-10 my-10">
-                <label htmlFor="" className=""></label>
+                <label htmlFor="" className="">Nombre</label>
             </form>
             <Reseñas />
             <Footer />
