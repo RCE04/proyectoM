df19c75cd1c48c79892209922b5d218ae23bcf00
RCO04 <rodrigogameelcrack@gmail.com>
2024-03-09 23:34:58 +0100

    eliminado y retoque de codigo para optimizacion

--- src/cuerpo.jsx
+++ src/cuerpo.jsx
@@ -8,7 +8,7 @@ export function Cuerpo() {
     ]
     return (
         <>
-            <div className='bg-cover bg-no-repeat bg-fixed bg-center w-screen bg-[url("/prueba2.jpg")] '>
+            <div className='bg-cover bg-no-repeat bg-fixed bg-center w-full bg-[url("/prueba2.jpg")] '>
                 <h1 className="text-4xl font-extrabold text-[#018edf] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                     LOS MEJORES MUEBLES DE ESPAÑA
                 </h1>
