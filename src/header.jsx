62b416176317a55266baef28da18f8c1436019f7
RCO04 <rodrigogameelcrack@gmail.com>
2024-03-09 14:52:11 +0100

    Reconfiguracion de imagen de cabecera

--- src/header.jsx
+++ src/header.jsx
@@ -5,7 +5,7 @@ export function Header() {
     return (
         <>
             <Cabecera />
-            <div className="bg-cover bg-no-repeat backdrop-blur-md backdrop-sepia bg-fixed bg-center h-screen bg-[url(' prueba2.jpg')]">
+            <div className='bg-cover bg-no-repeat backdrop-blur-md backdrop-sepia bg-fixed bg-center h-screen bg-[url("public/prueba2.jpg")] '>
                 <h1 className="text-4xl font-extrabold text-[#018edf] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                     LOS MEJORES MUEBLES DE ESPAÑA
                 </h1>
