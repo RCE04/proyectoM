11ceb058bd01d805c4f2934f3fed9534ef1db3a8
RCO04 <rodrigogameelcrack@gmail.com>
2024-03-10 16:10:07 +0100

    Retoque cabecera y finalizacion del retro P.I

--- src/cuerpo.jsx
+++ src/cuerpo.jsx
@@ -9,8 +9,8 @@ export function Cuerpo() {
     ]
     return (
         <>
-            <div className='bg-cover bg-no-repeat bg-fixed bg-center h-[60vh] bg-[url("/prueba13.jpg")] '>
-                <h1 className="text-4xl font-extrabold text-[#fff] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-[10vh]">
+            <div className='bg-cover bg-no-repeat bg-fixed bg-center h-[60vh] bg-[url("/prueba2.jpg")] '>
+                <h1 className="text-4xl font-extrabold text-[#036099] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-[10vh]">
                     LOS MEJORES MUEBLES DE ESPAÑA
                 </h1>
             </div >
@@ -19,7 +19,7 @@ export function Cuerpo() {
                 <div className="w-full md:mx-8 mt-4 md:mt-0 md:w-1/2 mx-auto text-pretty">
                     <h1 className="text-2xl md:text-4xl text-[#018edf] underline decoration-[#f3bc04] font-bold mb-2 md:mb-4">Buenos artesanos</h1>
                     <p className="text-base md:text-lg md:mb-4">Nuestros artesanos trabajan siempre buscando la perfección y el máximo detalle sin arruinar la veta de la madera</p>
-                    <Link to="/contactanos" className="inline-flex items-center justify-center mt-4 px-4 py-2 text-base md:text-lg font-semibold text-black border-2 border-black hover:bg-[#043c54] hover:text-white transition-all duration-200 focus:bg-[#043c54] focus:text-white" role="button">Sobre Nosotros</Link>
+                    <Link to="/madera" className="inline-flex items-center justify-center mt-4 px-4 py-2 text-base md:text-lg font-semibold text-black border-2 border-black hover:bg-[#043c54] hover:text-white transition-all duration-200 focus:bg-[#043c54] focus:text-white" role="button">Tipos de Madera</Link>
                 </div>
                 <img src='imgDetalles.jpg' className='w-full md:w-1/2 h-50 object-cover' alt="Detalles de Artesanía" />
             </section>
