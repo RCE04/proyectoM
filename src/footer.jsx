74148d985499e653e567176b8541d983d5fb15e9
RCO04 <rodrigogameelcrack@gmail.com>
2024-03-10 11:47:52 +0100

    Empezar retro y terminar footer y pagina de contacto

--- src/footer.jsx
+++ src/footer.jsx
@@ -21,16 +21,16 @@ export function Footer() {
                 <div className="md:w-1/3 w-full flex flex-col items-center mb-4 md:mb-0">
                     <div className="flex flex-col mx-16">
                         <img src="logo.png" alt="Logo" className="w-14 mx-auto" />
-                        <Link to="/" className="text-white  flex-col items-center mx-auto pt-0.5 hover:text-[#f3bc04] hover:underline decoration-current[#f3bc04]">Vanguardia Mueblera</Link>
+                        <Link to="https://www.google.com/intl/es/gmail/about/" className="text-white  flex-col items-center mx-auto pt-0.5 hover:text-[#f3bc04] hover:underline decoration-current[#f3bc04]">Vanguardia Mueblera</Link>
                     </div>
                 </div>
 
-                <div className="w-full md:w-[2px] md:h-15 bg-white/50 mb-4 md:"></div>
+                <div className="w-full md:w-[2px] md:h-15 bg-white/50 mb-4 md:mb-0"></div>
 
                 <div className="md:w-1/3 w-full flex flex-col items-center">
                     <div className="flex ml-16 mb-5 mr-2">
                         <img src="social.png" alt="WhatsApp" className="w-7" />
-                        <a href="" className="text-white  flex-col items-center mr-16 pt-0.5 hover:text-[#f3bc04] hover:underline decoration-current[#f3bc04]">+34 722-341-235</a>
+                        <a href="tel:+34722341235" className="text-white  flex-col items-center mr-16 pt-0.5 hover:text-[#f3bc04] hover:underline decoration-current[#f3bc04]">+34 722-341-235</a>
                     </div>
                     <div className="flex ml-16">
                         <img src="localizacion.png" alt="Icono Localizacion" className="w-7 mr-2" />
