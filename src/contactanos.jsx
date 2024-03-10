956895d26febb0f790c5926c11fe364117c4e8bf
RCO04 <rodrigogameelcrack@gmail.com>
2024-03-10 10:14:37 +0100

    terminar pagina de contacto

--- src/contactanos.jsx
+++ src/contactanos.jsx
@@ -40,7 +40,6 @@ function Contactanos() {
                     Enviar
                 </button>
             </form>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d4421.790785922489!2d-3.6975391781369273!3d40.41620927762158!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2ses!4v1710060508579!5m2!1ses!2ses" className=' mx-12 w-[90%] h-[100%]' allowfullscreen="" loading="fast" referrerpolicy="no-referrer-when-downgrade"></iframe>
             <Reseñas />
             <Footer />
         </>
